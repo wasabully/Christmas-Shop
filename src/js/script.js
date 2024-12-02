188d20ced3a921a3522aecbaa8565ce04221e0f8
Danil <dd.wasabully@gmail.com>
2024-12-02 17:10:31 +0300

    fix: fix bugs links

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const burgerIcon = document.querySelector('.burger-icon');
 const menuContent = document.querySelector('.burger-menu-content');
 const menuClose = document.querySelector('.menu-close');
+const menuLinks = document.querySelectorAll('.burger-menu-content a');
 
 burgerIcon.addEventListener('click', () => {
 	menuContent.classList.add('active');
@@ -8,4 +9,10 @@ burgerIcon.addEventListener('click', () => {
 
 menuClose.addEventListener('click', () => {
 	menuContent.classList.remove('active');
+});
+
+menuLinks.forEach((link) => {
+	link.addEventListener('click', () => {
+		menuContent.classList.remove('active');
+	});
 });
